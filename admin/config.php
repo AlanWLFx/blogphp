c2a6269ef4c5c3f4c965054b359e718543c33b6a
“Alan <ajf.goten@gmail.com>
2021-07-19 16:12:20 -0500

    Vistasy pagina inicio

---
+++ admin/config.php
@@ -0,0 +1,20 @@
+<?php
+define('RUTA', 'http://localhost/blogPractica/');
+
+$bd_config = array(
+    'basededatos' => 'blog',
+    'usuario' => 'root',
+    'pass' => 'ferrarisf19'
+);
+
+$blog_config = array(
+    'post_por_pagina' => '2',
+    'carpeta_imagenes' => 'imagenes/'
+);
+
+$blog_admin = array(
+    'usuario' => 'Alan',
+    'password' => '12345678'
+);
+
+?>
